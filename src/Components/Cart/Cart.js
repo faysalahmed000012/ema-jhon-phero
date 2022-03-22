2e6195491d3942f1829e93ae3365a6d64be86f6c
faysalahmed000012 <faysal000012@gmail.com>
2022-03-23 00:23:16 +0600

    end of day 2

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart }) => {
+  let total = 0;
+  let shipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  const tax = (total * 0.1).toFixed(3);
+  const grandTotal = total + shipping + Number(tax);
   return (
-    <div>
-      <h3>This is for cart</h3>
-      <h5>Items: {cart.length}</h5>
+    <div className="cart">
+      <h3>Order Summary</h3>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h5>Grand Total: ${grandTotal}</h5>
+      <button></button> <br />
+      <button></button>
     </div>
   );
 };
